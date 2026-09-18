bb344e358231bb80483dcec193260a799a50197a
GabVC <gabr.capacio@gmail.com>
2026-09-18 09:22:17 +0800

    Add Participant Intake Schema

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,6 +6,7 @@ from .learning_content import LearningContent
 from .learning_strand import LearningStrand
 from .lri_test import LRITest, LRITestItem
 from .lri_test_attempt import LRITestAttempt, LRITestAttemptAnswer
+from .participant_intake import ParticipantIntake
 from .refresh_token import RefreshToken
 from .strand_test import StrandTest, StrandTestItem, StrandTestItemOption
 from .strand_test_attempt import StrandTestAttempt, StrandTestAttemptAnswer
@@ -28,6 +29,7 @@ __all__ = [
     "LearningStrand",
     "Lesson",
     "Module",
+    "ParticipantIntake",
     "RefreshToken",
     "StrandTest",
     "StrandTestAttempt",
